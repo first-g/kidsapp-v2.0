26d47f772f03458505d36ee94dbf4255f3f87285
azimust <woody.azimbek@gmail.com>
2023-09-10 11:05:34 +0600

    add animals page

---
+++ src/components/Animals/Animals.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Animals.css'
+import { BsDoorOpenFill } from 'react-icons/bs'
+import { Link } from 'react-router-dom'
+
+const Animals = () => {
+    return (
+        <div className='animals'>
+            <div className="animals__container">
+                <div className="animals__body">
+                    <div className="animals__circle"></div>
+                    <div className="animals__content-section">
+                        <div className="animals__title">
+                            <Link to='/' className="leave">
+                                <BsDoorOpenFill />
+                            </Link>
+                            <h1>Жаныбарлар</h1>
+                        </div>
+                        <div className="animals__cards">
+                            {/* Здесь будут карточки */}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Animals

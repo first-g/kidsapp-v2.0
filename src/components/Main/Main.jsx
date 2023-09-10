52dd1fa05e5cb4064a1ab660bf592d4e165ee25c
azimust <woody.azimbek@gmail.com>
2023-09-10 09:57:40 +0600

    add router Links

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Main.css'
+import { Link } from 'react-router-dom'
 
 const Main = () => {
     return (
@@ -12,22 +13,22 @@ const Main = () => {
                     </div>
 
                     <div className="main__right-section">
-                        <div className="main__button">
+                        <Link to='zhan' className="main__button">
                             <div className="main__button-image"></div>
                             <div className="main__button-text">Жаныбарлар</div>
-                        </div>
-                        <div className="main__button">
+                        </Link>
+                        <Link to='test' className="main__button">
                             <div className="main__button-image"></div>
                             <div className="main__button-text">Тест</div>
-                        </div>
-                        <div className="main__button">
+                        </Link>
+                        <Link to='tamgalar' className="main__button">
                             <div className="main__button-image"></div>
                             <div className="main__button-text">Тамгалар</div>
-                        </div>
-                        <div className="main__button">
+                        </Link>
+                        <Link to='zhomoktor' className="main__button">
                             <div className="main__button-image"></div>
                             <div className="main__button-text">Жомоктор</div>
-                        </div>
+                        </Link>
                     </div>
                 </div>
             </div>

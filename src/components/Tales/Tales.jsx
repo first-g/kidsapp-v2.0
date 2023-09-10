af2d6f2b689ca60bb53ce662bc5e56cd5f526275
azimust <woody.azimbek@gmail.com>
2023-09-10 11:06:07 +0600

    add tales page

---
+++ src/components/Tales/Tales.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Tales.css'
+import { BsDoorOpenFill } from 'react-icons/bs'
+import { Link } from 'react-router-dom'
+
+const Tales = () => {
+    return (
+        <div className='tales'>
+            <div className="tales__container">
+                <div className="tales__body">
+                    <div className="tales__circle"></div>
+                    <div className="tales__content-section">
+                        <div className="tales__title">
+                            <Link to='/' className="leave">
+                                <BsDoorOpenFill />
+                            </Link>
+                            <h1>Жомоктор</h1>
+                        </div>
+                        <div className="tales__cards">
+                            {/* Здесь будут карточки */}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Tales

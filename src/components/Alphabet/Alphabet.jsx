c846cddad4abed257c24ab64368d4057b2b0dab4
azimust <woody.azimbek@gmail.com>
2023-09-10 11:05:24 +0600

    add alphabet page

---
+++ src/components/Alphabet/Alphabet.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Alphabet.css'
+import { BsDoorOpenFill } from 'react-icons/bs'
+import { Link } from 'react-router-dom'
+
+const Alphabet = () => {
+    return (
+        <div className='alphabet'>
+            <div className="alphabet__container">
+                <div className="alphabet__body">
+                    <div className="alphabet__circle"></div>
+                    <div className="alphabet__content-section">
+                        <div className="alphabet__title">
+                            <Link to='/' className="leave">
+                                <BsDoorOpenFill />
+                            </Link>
+                            <h1>Тамгалар</h1>
+                        </div>
+                        <div className="alphabet__cards">
+                            {/* Здесь будут карточки */}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Alphabet
